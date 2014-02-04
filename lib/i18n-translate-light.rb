6b92443bb76d993153ab7a9819031d70f96b94ff
Winfield Peterson <winfield.peterson@gmail.com>
2014-02-04 12:33:48 -0500

    Make gem a proper Rails engine as asset provider
    Provide the i18n-translate-light JS asset.

---
+++ lib/i18n-translate-light.rb
@@ -0,0 +1,8 @@
+module I18N
+  module Translate
+    module Light
+      class Engine < ::Rails::Engine
+      end
+    end
+  end
+end
---
+++ lib/i18n-translate-light/version.rb
@@ -0,0 +1,7 @@
+module I18N
+  module Translate
+    module Light
+      VERSION = "0.0.1"
+    end
+  end
+end
